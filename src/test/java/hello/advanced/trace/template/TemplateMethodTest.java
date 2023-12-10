a99fe812455feb44bea03a142d6444e43d1825b3
Minjae Kim <wnsqud7070@gmail.com>
2023-12-10 21:22:10 +0900

    :white_check_mark: 템플릿 메서드 패턴 적용

---
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -0,0 +1,82 @@
+package hello.advanced.trace.template;
+
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.code.AbstractTemplate;
+import hello.advanced.trace.threadlocal.code.SubClassLogic1;
+import hello.advanced.trace.threadlocal.code.SubClassLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class TemplateMethodTest {
+
+    @Test
+    void templateMethodV0(){
+        logic1();
+        logic2();
+    }
+
+    private void logic1(){
+        long startTime = System.currentTimeMillis();
+        //비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행");
+        //비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+
+    }
+
+    private void logic2(){
+        long startTime = System.currentTimeMillis();
+        //비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행");
+        //비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+
+    }
+
+    /**
+     * <템플릿 메서드 패턴>
+     * 1. 공통부분은 상속받은 클래스
+     * 2. 변하는 부분은 추상메서드로
+     */
+    @Test
+    void templateMethodV1(){
+        AbstractTemplate template1  = new SubClassLogic1();
+        template1.execute();
+
+        AbstractTemplate template2  = new SubClassLogic2();
+        template2.execute();
+    }
+
+
+    /**
+     * 익명 내부 클래스 사용으로 위에 V1 보완
+     */
+    @Test
+    void templateMethodV2(){
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+
+            }
+        };
+        template1.execute();
+
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직2 실행");
+
+            }
+        };
+        template2.execute();
+
+    }
+
+}
